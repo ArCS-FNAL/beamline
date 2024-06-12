8534ae29a3afece365ecdfa8980b0127c1918fb5
Marco Del Tutto <mdeltutt@fnal.gov>
2024-06-12 13:33:11 -0500

    Update username

--- Jobsubmit.sh
+++ Jobsubmit.sh
@@ -1,7 +1,2 @@
-jobsub_submit --debug -G lariat --memory=500MB --expected-lifetime=20h -N 2 -l '+SingularityImage=\"/cvmfs/singularity.opensciencegrid.org/fermilab/fnal-wn-sl7:latest\"' --append_condor_requirements='(TARGET.HAS_SINGULARITY=?=true)' --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC,OFFSITE --tar_file_name dropbox:///pnfs/lariat/persistent/users/suprajab/G4beamline-3.06_7june2022.tar -f /pnfs/lariat/resilient/users/suprajab/pos60Amps/MergeTrees.py -f /pnfs/lariat/resilient/users/suprajab/pos60Amps/LAriaT_13degProdxn_10degAna_SurveyedGeom_10000jobsof35k_64GeV_pos60Amps.in -f /pnfs/lariat/resilient/users/suprajab/pos60Amps/JGG.in -f /pnfs/lariat/resilient/users/suprajab/pos60Amps/MIPPFieldMapForG4Beamline.txt --use-cvmfs-dropbox file://$PWD/Script.sh
-
-
-
-
-
+jobsub_submit --debug -G lariat --memory=500MB --expected-lifetime=20h -N 2 -l '+SingularityImage=\"/cvmfs/singularity.opensciencegrid.org/fermilab/fnal-wn-sl7:latest\"' --append_condor_requirements='(TARGET.HAS_SINGULARITY=?=true)' --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC,OFFSITE --tar_file_name dropbox:///pnfs/lariat/persistent/users/mdeltutt/G4beamline/G4beamline-3.06-06102024.tar -f $PWD/MergeTrees.py -f $PWD/LAriaT_13degProdxn_10degAna_SurveyedGeom_10000jobsof35k_64GeV_pos60Amps.in -f $PWD/JGG.in -f $PWD/jgg_field_map.txt --use-cvmfs-dropbox file://$PWD/Script.sh
 
--- Script.sh
+++ Script.sh
@@ -16,7 +16,7 @@ setup g4surface v2_1_1
 source ${CONDOR_DIR_INPUT}/G4beamline-3.06-06102024/G4beamline-3.06/bin/g4bl-setup.sh
 source ${CONDOR_DIR_INPUT}/G4beamline-3.06-06102024/G4beamline-3.06/root/bin/thisroot.sh
 
-jobsize=35000
+jobsize=100 #35000
 SUBspillcount=10000
 first=$((${PROCESS}*${jobsize}))
 last=$(( ${first} + $jobsize - 1 ))
