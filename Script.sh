73430c009d0628fdc40c0c6b8b8d54896d35d061
Marco Del Tutto <mdeltutt@fnal.gov>
2024-06-12 13:25:46 -0500

    Run on local files on the grid

--- Script.sh
+++ Script.sh
@@ -13,10 +13,10 @@ setup g4tendl v1_3_2
 setup g4photon v5_5
 setup g4radiative v5_4
 setup g4surface v2_1_1
-source ${CONDOR_DIR_INPUT}/G4beamline-3.06_7june2022/G4beamline-3.06/bin/g4bl-setup.sh
-source ${CONDOR_DIR_INPUT}/G4beamline-3.06_7june2022/G4beamline-3.06/root/bin/thisroot.sh
+source ${CONDOR_DIR_INPUT}/G4beamline-3.06-06102024/G4beamline-3.06/bin/g4bl-setup.sh
+source ${CONDOR_DIR_INPUT}/G4beamline-3.06-06102024/G4beamline-3.06/root/bin/thisroot.sh
 
-jobsize=100 #35000
+jobsize=35000
 SUBspillcount=10000
 first=$((${PROCESS}*${jobsize}))
 last=$(( ${first} + $jobsize - 1 ))
@@ -26,10 +26,28 @@ echo "first = $first"
 echo "last = $last"
 SUBSPILL=$((${PROCESS}+1 ))
 
+echo
+echo "running echo CONDOR_DIR_INPUT"
+echo ${CONDOR_DIR_INPUT}
+echo
+
+echo "Copy relevat files here"
+cp ${CONDOR_DIR_INPUT}/*.in .
+cp ${CONDOR_DIR_INPUT}/*.txt .
+cp ${CONDOR_DIR_INPUT}/*.py .
+echo "Available files in this directory:"
+ls -lh .
+
+echo ${INPUT_TAR_DIR_LOCAL}
+ls -lh ${INPUT_TAR_DIR_LOCAL}
+echo
+
 echo ${CONDOR_DIR_INPUT}
 ls -lh ${CONDOR_DIR_INPUT}
+echo 
 
-g4bl ${CONDOR_DIR_INPUT}/LAriaT_13degProdxn_10degAna_SurveyedGeom_10000jobsof35k_64GeV_pos60Amps.in first=$first last=$last
+echo "Running g4bl"
+g4bl LAriaT_13degProdxn_10degAna_SurveyedGeom_10000jobsof35k_64GeV_pos60Amps.in first=$first last=$last
 if [[ $? -ne 0 ]]; then
    echo "The g4bl command failed"
    exit 1
@@ -37,8 +55,10 @@ fi
 ls -lrth
 
 chmod 777 sim_LAriaT_13degProdxn_10degAna_SurveyedGeom_10000jobsof35k_64GeV_pos60Amps.root
-chmod 777 ${CONDOR_DIR_INPUT}/MergeTrees.py
-${CONDOR_DIR_INPUT}/MergeTrees.py sim_LAriaT_13degProdxn_10degAna_SurveyedGeom_10000jobsof35k_64GeV_pos60Amps.root --subspillnumber $SUBSPILL --subspillcount $SUBspillcount
+chmod 777 /MergeTrees.py
+
+echo "Running MergeTrees.py"
+MergeTrees.py sim_LAriaT_13degProdxn_10degAna_SurveyedGeom_10000jobsof35k_64GeV_pos60Amps.root --subspillnumber $SUBSPILL --subspillcount $SUBspillcount
 if [[ $? -ne 0 ]]; then
    echo "Running MergeTrees.py failed"
    exit 1
@@ -51,12 +71,17 @@ REALUSER=`basename ${X509_USER_PROXY} .proxy | grep -o -P '(?<=_).*(?=_)'`
 echo '$USER: ' $USER
 echo '$REALUSER: ' $REALUSER
 
-ifdh cp MergedAtStartLinesim_LAriaT_13degProdxn_10degAna_SurveyedGeom_10000jobsof35k_64GeV_pos60Amps.root /pnfs/lariat/persistent/users/suprajab/BeamLineSimOutputs/17may2023/pos60Amps/MergedAtStartLinesim_LAriaT_13degProdxn_10degAna_SurveyedGeom_10000jobsof35k_64GeV_pos60Amps$SUBSPILL.root
+ifdh cp sim_LAriaT_13degProdxn_10degAna_SurveyedGeom_10000jobsof35k_64GeV_pos60Amps.root /pnfs/lariat/persistent/users/mdeltutt/BeamLineSimOutputs/pos60Amps/sim_LAriaT_13degProdxn_10degAna_SurveyedGeom_10000jobsof35k_64GeV_pos60Amps_$SUBSPILL.root
+if [[ $? -ne 0 ]]; then
+   echo "Copying of the g4bl output ROOT file failed"
+   exit 1
+fi
+ifdh cp MergedAtStartLinesim_LAriaT_13degProdxn_10degAna_SurveyedGeom_10000jobsof35k_64GeV_pos60Amps.root /pnfs/lariat/persistent/users/mdeltutt/BeamLineSimOutputs/pos60Amps/MergedAtStartLinesim_LAriaT_13degProdxn_10degAna_SurveyedGeom_10000jobsof35k_64GeV_pos60Amps_$SUBSPILL.root
 if [[ $? -ne 0 ]]; then
    echo "Copying of the output ROOT file failed"
    exit 1
 fi
-ifdh cp MergedAtStartLinesim_LAriaT_13degProdxn_10degAna_SurveyedGeom_10000jobsof35k_64GeV_pos60Amps.pickle /pnfs/lariat/persistent/users/suprajab/BeamLineSimOutputs/17may2023/pos60Amps/MergedAtStartLinesim_LAriaT_13degProdxn_10degAna_SurveyedGeom_10000jobsof35k_64GeV_pos60Amps$SUBSPILL.pickle
+ifdh cp MergedAtStartLinesim_LAriaT_13degProdxn_10degAna_SurveyedGeom_10000jobsof35k_64GeV_pos60Amps.pickle /pnfs/lariat/persistent/users/mdeltutt/BeamLineSimOutputs/pos60Amps/MergedAtStartLinesim_LAriaT_13degProdxn_10degAna_SurveyedGeom_10000jobsof35k_64GeV_pos60Amps_$SUBSPILL.pickle
 if [[ $? -ne 0 ]]; then
    echo "Copying of the output pickle file failed"
    exit 1
