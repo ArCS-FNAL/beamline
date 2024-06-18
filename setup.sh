d9f3ff280cfb17c881f3348a7657014112afe635
Marco Del Tutto <mdeltutt@fnal.gov>
2024-06-18 10:41:50 -0500

    Add local setup file

---
+++ setup.sh
@@ -0,0 +1,18 @@
+source /cvmfs/larsoft.opensciencegrid.org/setup_larsoft.sh
+source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups.sh
+setup ifdhc
+setup ifdhc
+setup g4abla v3_1
+setup g4emlow v7_9_1
+setup g4nuclide v2_2
+setup g4incl v1_0
+setup g4neutronxs v1_4
+setup g4particlexs v2_1
+setup g4pii v1_3
+setup g4tendl v1_3_2
+setup g4photon v5_5
+setup g4radiative v5_4
+setup g4surface v2_1_1
+source /pnfs/lariat/persistent/users/mdeltutt/G4beamline/G4beamline-3.06/bin/g4bl-setup.sh
+source /pnfs/lariat/persistent/users/mdeltutt/G4beamline/G4beamline-3.06/root/bin/thisroot.sh
+
