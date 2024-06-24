0a2308da95c62a3143b502fc68c527577cd71ce2
Marco Del Tutto <marco.deltutto@gmail.com>
2024-06-24 13:23:59 -0500

    Reorganize into folders

--- Simulation/make_skimmed_trees.py
+++ Simulation/make_skimmed_trees.py
@@ -18,7 +18,7 @@ arrays = file[name].arrays()
 
 with uproot.recreate(out_file_name) as output_file:
     
-    for det in ['Det1', 'Det2', 'Det3', 'Det4', 'Det5', 'Det6', 'Det7', 'TOFus', 'TOFds']:
+    for det in ['Det1', 'Det2', 'Det3', 'Det4', 'Det5', 'Det6', 'Det7', 'Det8', 'TOFus', 'TOFds']:
         
         if f'TrackPresent{det}' not in arrays:
           continue
