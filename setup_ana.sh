0f7e10c2b18414524634e57fce8ea85a842e40d0
Marco Del Tutto <mdeltutt@fnal.gov>
2024-06-24 13:36:25 -0500

    Remove old

--- setup_ana.sh
+++
@@ -1,7 +0,0 @@
-source /cvmfs/larsoft.opensciencegrid.org/setup_larsoft.sh
-source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups.sh
-setup ifdhc
-setup python v3_9_15
-#source analysis-env/bin/activate
-source ana-env/bin/activate
-
