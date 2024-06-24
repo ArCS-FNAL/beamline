3b62556901f31c28a5393c2a1319813a2d62d768
Marco Del Tutto <mdeltutt@fnal.gov>
2024-06-24 13:32:04 -0500

    More requirements.txt setup_ana.sh in Analysis/

---
+++ Analysis/setup_ana.sh
@@ -0,0 +1,7 @@
+source /cvmfs/larsoft.opensciencegrid.org/setup_larsoft.sh
+source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups.sh
+setup ifdhc
+setup python v3_9_15
+#source analysis-env/bin/activate
+source ana-env/bin/activate
+
