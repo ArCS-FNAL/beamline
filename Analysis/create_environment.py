8a3d3f3ab2f0d71603ae6489a72496c9418d5dcc
Marco Del Tutto <marco.deltutto@gmail.com>
2024-08-23 07:17:59 -0500

    Update create_environment.py

--- Analysis/create_environment.py
+++ Analysis/create_environment.py
@@ -5,6 +5,13 @@
 # Define the directory for the virtual environment
 ENV_DIR="analysis-env"
 
+echo "Creating environment $ENV_DIR"
+
+# Set up python
+source /cvmfs/larsoft.opensciencegrid.org/setup_larsoft.sh
+source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups.sh
+setup python v3_9_15
+
 # Create a virtual environment
 python3 -m venv $ENV_DIR
 
