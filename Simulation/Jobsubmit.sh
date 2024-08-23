e1eb9f0925ee384a9b9044c2e2c17b631eb619b2
Marco Del Tutto <marco.deltutto@gmail.com>
2024-08-23 10:57:35 -0500

    Remove incorrect file for config09

--- Simulation/Jobsubmit.sh
+++ Simulation/Jobsubmit.sh
@@ -1,7 +1,9 @@
 export BOOKDIR=/pnfs/lariat/resilient/users/mdeltutt/beamline_bookdir
-export OUTDIR=/pnfs/lariat/persistent/users/mdeltutt/BeamLineSimOutputs/pos60Amps/config09_1/
-export G4BNBINPUT=arcs_beamline_config09.in
+export OUTDIR=/pnfs/lariat/persistent/users/mdeltutt/BeamLineSimOutputs/pos60Amps/config08_1/
+export G4BNBINPUT=arcs_beamline_config08.in
 export OUTFILE=sim_arcs_beamline
+
+# Standard production is done wih 10000 jobs with 30000 pions each
 
 # Number of jobs to submit
 export NJOBS=2 #10000
@@ -9,7 +11,7 @@ export NJOBS=2 #10000
 # Number of pions on target per job
 export JOBSIZE=100 #30000
 
-# Bending magnets fields
+# LArIAT bending magnets fields
 export BFIELD=0 # magnets off
 #export BFIELD=-0.2121 # 60 Amps pos
 # export BFIELD=-0.3361 # 100 Amps pos
