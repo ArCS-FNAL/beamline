fdbbe1401b6c639f0d8e82fef9fa75303057d27d
gcicogna <giulia.cicogna@studio.unibo.it>
2024-09-22 20:55:51 -0500

    Update on config
    JGG on
    LArIAT on at 60 Amp neg

--- Simulation/Jobsubmit.sh
+++ Simulation/Jobsubmit.sh
@@ -1,5 +1,5 @@
 export BOOKDIR=/pnfs/lariat/resilient/users/gcicogna/beamline_bookdir
-export OUTDIR=/pnfs/lariat/persistent/users/gcicogna/BeamLineSimOutputs/JGGoff_LArIAToff/config10_1/
+export OUTDIR=/pnfs/lariat/persistent/users/gcicogna/BeamLineSimOutputs/JGGon_LArIATon/config10_1_neg60Amps/
 export G4BNBINPUT=arcs_beamline_config10.in
 export OUTFILE=sim_arcs_beamline
 
@@ -12,10 +12,10 @@ export NJOBS=6000
 export JOBSIZE=30000
 
 # LArIAT bending magnets fields
-export BFIELD=0 # magnets off
+#export BFIELD=0 # magnets off
 #export BFIELD=-0.2121 # 60 Amps pos
 #export BFIELD=-0.3361 # 100 Amps pos
-# export BFIELD=0.2121 # 60 Amps neg
+export BFIELD=0.2121 # 60 Amps neg
 # export BFIELD=0.3361 # 100 Amps neg
 
 # Copy all neded files to a folder on resilient
