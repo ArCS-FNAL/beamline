625b80b165ccd32f02776ae86a10930504e9c14a
gcicogna <giulia.cicogna@studio.unibo.it>
2024-09-24 09:08:42 -0500

    Update
    Config magnets off

--- Simulation/Jobsubmit.sh
+++ Simulation/Jobsubmit.sh
@@ -1,5 +1,5 @@
 export BOOKDIR=/pnfs/lariat/resilient/users/gcicogna/beamline_bookdir
-export OUTDIR=/pnfs/lariat/persistent/users/gcicogna/BeamLineSimOutputs/JGGon_LArIATon/config10_1_neg100Amps/
+export OUTDIR=/pnfs/lariat/persistent/users/gcicogna/BeamLineSimOutputs/JGGoff_LArIAToff/config10_1/
 export G4BNBINPUT=arcs_beamline_config10.in
 export OUTFILE=sim_arcs_beamline
 
@@ -12,11 +12,11 @@ export NJOBS=6000
 export JOBSIZE=30000
 
 # LArIAT bending magnets fields
-#export BFIELD=0 # magnets off
+export BFIELD=0 # magnets off
 #export BFIELD=-0.2121 # 60 Amps pos
 #export BFIELD=-0.3361 # 100 Amps pos
 #export BFIELD=0.2121 # 60 Amps neg
-export BFIELD=0.3361 # 100 Amps neg
+#export BFIELD=0.3361 # 100 Amps neg
 
 # Copy all neded files to a folder on resilient
 mkdir -p ${OUTDIR}
